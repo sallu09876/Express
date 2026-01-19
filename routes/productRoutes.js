b557dcd6b30322fdf5ddfaa0469c51b3c5266281
Salman Rasheed <sallu2004mkt@gmail.com>
2026-01-19 12:59:32 +0530

    ex

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -11,11 +11,7 @@ router.post("/add", controller.upload.single("image"), controller.addProduct);
 
 /* Edit */
 router.get("/edit/:id", controller.editProductPage);
-router.post(
-  "/edit/:id",
-  controller.upload.single("image"),
-  controller.updateProduct,
-);
+router.post("/edit/:id", controller.upload.single("image"), controller.updateProduct);
 
 /* Delete */
 router.get("/delete/:id", controller.deleteProduct);
