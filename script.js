c23300094a7b1fabe4f4d4232c2aa35bb33f55f6
Salman Rasheed <sallu2004mkt@gmail.com>
2026-01-23 11:05:02 +0530

    s

---
+++ script.js
@@ -0,0 +1,26 @@
+// to find the second largest number in an array
+// let num = [10, 35, -30, 45, -50, 50, -20]
+// let num = [-2, -4, -1, -7, -5, 0]
+
+// num.sort(function(a, b){return b-a}); 
+// console.log(num);
+// console.log("The second largest number is: " + num[1]); 
+
+// without sorting
+// let largest = -Infinity;
+// let secondLargest = -Infinity;
+
+// for (let i = 0; i < num.length; i++) { 
+//     if (num[i] > largest) { 
+//         secondLargest = largest; 
+//         largest = num[i]; 
+//     } else if (num[i] > secondLargest && num[i] !== largest) { 
+//         secondLargest = num[i]; 
+//     }
+// }
+// console.log("The second largest number is: " + secondLargest);
+
+
+// HW :- Palindrome, Amstrong, To Do App with Express
+// js working, code exexcution, memmory allocation, call stack
+// react working 
