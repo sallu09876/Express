c8573e08405dd8f2577f65d63b19cceaabaced3a
Salman Rasheed <sallu2004mkt@gmail.com>
2026-02-04 09:37:16 +0530

    ex

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,13 +1,18 @@
 const express = require("express");
-// const router = express.Router();
-import { Router } from 'express';
-const router = Router();
+const router = express.Router();
+// import { Router } from 'express';
+// const router = Router();
 const mailController = require("../controllers/mail.controller");
+const authController = require("../controllers/auth.controller");
+
+// Auth routes
+router.post("/register", authController.register);
+router.post("/login", authController.login);
 
 // OTP routes
-router.get("/otp", mailController.otpPage);             // GET page
-router.post("/otp", mailController.sendOtp);            // POST email to send OTP
-router.post("/verify-otp", mailController.verifyOtp);  // POST OTP verification
+router.get("/otp", mailController.otpPage); // GET page
+router.post("/otp", mailController.sendOtp); // POST email to send OTP
+router.post("/verify-otp", mailController.verifyOtp); // POST OTP verification
 
-// module.exports = router;
-export default router;
+module.exports = router;
+// export default router;
